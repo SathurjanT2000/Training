e7efa21f1ce0f97913b9f8d296455a19c7832c86
T Sathurjan <sajisathurjan12@gmail.com>
2022-04-11 18:16:53 +0000

    #19 got rid of login page and added login to index

--- application/routes.py
+++ application/routes.py
@@ -4,16 +4,16 @@ from application.forms import LogInForm
 from flask import render_template
 
 @app.route('/', methods=['GET','POST'])
-def login():
+def index():
     all_trainers = Trainers.query.all()
     all_trainees = Trainees.query.all()
     login_form = LogInForm()
     if Trainers.query.get(login_form.username.data) != None:
-        return render_template('index.html', all_trainers=all_trainers, all_trainees=all_trainees, login_form=login_form)
+        return render_template('trainer_home.html', all_trainers=all_trainers, all_trainees=all_trainees, login_form=login_form)
     elif Trainees.query.get(login_form.username) != None:
-        return render_template('index(trainee).html', all_trainees=all_trainees, all_trainers=all_trainers, login_form=login_form)
+        return render_template('trainee_home.html', all_trainees=all_trainees, all_trainers=all_trainers, login_form=login_form)
     else:
-        return render_template('login.html', login_form=login_form)
+        return render_template('index.html', login_form=login_form)
 
 
     
