84bf21933160443f7f1dd12b0bb7e8a1c302a076
T Sathurjan <sajisathurjan12@gmail.com>
2022-04-11 13:55:51 +0000

    #12 implemented try-except method

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,18 +1,27 @@
 from flask_testing import TestCase
 from application import app, db
 from application.models import Trainees, Trainers
-from Secrets import SQLALCHEMY_TEST_DATABASE_URI
 from flask import url_for
 import uuid
 
 class TestBase(TestCase):
     def create_app(self):
-        app.config.update(
-            SQLALCHEMY_DATABASE_URI= SQLALCHEMY_TEST_DATABASE_URI,
-            SECRET_KEY = str(uuid.uuid4),
-            WTF_CSRF_ENABLED=False
-        )
-        return app
+        try:
+            from Secrets import SQLALCHEMY_TEST_DATABASE_URI
+            app.config.update(
+                SQLALCHEMY_DATABASE_URI= SQLALCHEMY_TEST_DATABASE_URI,
+                SECRET_KEY = str(uuid.uuid4),
+                WTF_CSRF_ENABLED=False
+            )
+            return app
+        except:
+            from os import getenv
+            app.config.update(
+                SQLALCHEMY_DATABASE_URI= getenv("SQLALCHEMY_DATABASE_URI"),
+                SECRET_KEY = str(uuid.uuid4),
+                WTF_CSRF_ENABLED=False
+            )
+            return app
 
 class testViews(TestBase):
     def test_home_get(self):
