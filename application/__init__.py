8b20a0fb89dba37fbda52ffb6a4c976e93a4f804
T Sathurjan <sajisathurjan12@gmail.com>
2022-04-11 14:01:00 +0000

    #12 countinuation from last commit

--- application/__init__.py
+++ application/__init__.py
@@ -4,8 +4,13 @@ from Secrets import SQLALCHEMY_DATABASE_URI
 import uuid
 
 app = Flask(__name__)
+try:
+    from Secrets import SQLALCHEMY_DATABASE_URI
+    app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+except:
+    from os import getenv
+    app.config['SQLALCHEMY_DATABASE_URI'] = "SQLALCHEMY_DATABASE_URI"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SECRET_KEY'] = str(uuid.uuid4)
 
 db = SQLAlchemy(app)
